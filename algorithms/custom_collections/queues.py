408fc5de8020a8c449258cea3215e703ea2eb87e
Vikas Z <8222698+vikasbz@users.noreply.github.com>
2021-10-08 14:42:58 +0530

    add Queue example

--- algorithms/custom_collections/queues.py
+++ algorithms/custom_collections/queues.py
@@ -12,5 +12,24 @@ class Queue:
     def get(self):
         return self.items.pop()
 
-    def size(self):
+    @property
+    def size(self) -> int:
         return len(self.items)
+
+
+if __name__ == "__main__":
+    q = Queue()
+
+    print(q.size)
+
+    q.put(11)
+    q.put("Aa")
+    q.put(22)
+    q.put(33)
+    q.put("Bb")
+
+    print(q.items)
+
+    print(q.get())
+    print(q.get())
+    print(q.items)
